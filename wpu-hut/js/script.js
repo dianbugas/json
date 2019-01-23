0ee700474c012e639a0e20f9c48429ac0a33ca32
dianynf <dianbugas@gmail.com>
2019-01-23 09:43:29 +0700

    menu

--- wpu-hut/js/script.js
+++ wpu-hut/js/script.js
@@ -4,3 +4,7 @@ $.getJSON('data/pizza.json', function (data) {
         $('#daftar-menu').append('<div class="col-md-4"><div class="card-mb-3"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5>Rp. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan sekarang</a></div></div></div>');
     });
 });
+
+$('.nav-link').on('click', function () {
+
+});
